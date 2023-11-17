85ef22b3f3f6813302fb534bb58fc0aa8b4bd372
PYTNAG <sgds.2020.nag@yandex.ru>
2023-11-17 19:16:22 +0500

    fix created_at fields

--- back/db/migration/000001_init.up.sql
+++ back/db/migration/000001_init.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE "rfcs" (
   "id" serial UNIQUE PRIMARY KEY,
   "author_id" integer REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
   "message" text NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT 'now()',
+  "created_at" timestamp with time zone NOT NULL DEFAULT now(),
   "archived" boolean NOT NULL DEFAULT FALSE
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE "comments" (
   "id" serial UNIQUE PRIMARY KEY,
   "author_id" integer REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
   "rfc_id" integer REFERENCES "rfcs" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-  "created_at" timestamp NOT NULL DEFAULT 'now()'
+  "created_at" timestamp with time zone NOT NULL DEFAULT now()
 );
 
 CREATE INDEX ON "users" ("id");
